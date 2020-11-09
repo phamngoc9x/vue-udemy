80f159e29d9cd8349058fac95a46aa837cb19a9e
pham.van.ngoc <pham.van.ngoc@B121645-mb.local>
2020-11-09 09:04:34 +0700

    update starting setup

---
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <TheHeader />
+    <button @click="setSelectedComponent('ActiveGoals')">Active Goals</button>
+    <button @click="setSelectedComponent('ManageGoals')">Manage Goals</button>
+    <!-- <ActiveGoals v-if="selectedComponent ==='ActiveGoals'"/>
+    <ManageGoals v-if="selectedComponent ==='ManageGoals'"/> -->
+    <keep-alive> 
+      <component :is="selectedComponent"></component>
+    </keep-alive> 
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
+
+export default {
+  components: {
+    TheHeader, ActiveGoals, ManageGoals
+  },
+  data() {
+    return {
+      selectedComponent: 'ActiveGoals',
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp
+    }
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
