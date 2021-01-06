de70362dd276c992d8a56d2f6fdc933e23a63f93
pham.van.ngoc <pham.van.ngoc@sun-asterisk.com>
2021-01-06 14:47:50 +0700

    update code

---
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>
+    <button @click="inc">Add 2</button>
+    <button @click="increase({value:11 })">Add 11</button>
+  </span>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  methods: {
+    // addOne () {
+    //   this.$store.dispatch('increase', {value: 10});
+    // }
+    // ...mapActions(['increment', 'increase'])
+    ...mapActions({
+      inc: 'increment',
+      increase: 'increase'
+    })
+  }
+}
+</script>
---
+++ vuex-01-starting-setup/src/components/UserAuth.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <button>Login</button>
+    <button>Logout</button>
+  </div>
+</template>
