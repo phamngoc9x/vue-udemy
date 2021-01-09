6c9d1aac3636f4e495515e8b979a27859ae72a40
pham.van.ngoc <pham.van.ngoc@sun-asterisk.com>
2021-01-09 16:05:15 +0700

    update code

---
+++ vuex-11-a-challenge-starting-code/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/nav/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader
+  },
+  methods: {
+   
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
