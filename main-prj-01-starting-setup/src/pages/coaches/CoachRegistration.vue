4574afbe1dd64fa227893764f665dc5df5477b89
pham.van.ngoc <pham.van.ngoc@sun-asterisk.com>
2021-01-27 10:22:08 +0700

    update

--- main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,28 @@
 <template>
-  Register
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form @save-data="saveData"></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm
+  },
+  computed: {
+    isCoach() {
+      return this.$store.getters['coaches/isCoach'];
+    }
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.replace('coaches')
+    }
+  }
+}
+</script>
--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -7,7 +7,7 @@
       <base-card>
       <div class="controls">
         <base-button mode="outline">Refresh</base-button>
-        <base-button link to="/register">
+        <base-button v-if="!isCoach" link to="/register">
           Register as Coach
         </base-button>
       </div>
@@ -63,6 +63,9 @@ export default {
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches']
+    },
+    isCoach() {
+      return this.$store.getters['coaches/isCoach'];
     }
   },
   methods: {
