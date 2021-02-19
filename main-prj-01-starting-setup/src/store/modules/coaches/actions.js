d2506ac615a036095e015ebf9ecc1d2b5ef13fea
pham.van.ngoc <pham.van.ngoc@sun-asterisk.com>
2021-02-19 12:54:39 +0700

    update project

--- main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -1,13 +1,55 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     }
-    context.commit('registerCoach', coachData);
+
+    const response = await fetch(`https://vue-http-demo-df3df-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData),
+    });
+
+    // const reponseData = await response.json();
+
+    if(!response.ok) {
+      // error
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://vue-http-demo-df3df-default-rtdb.firebaseio.com/coaches.json`
+    );
+
+    const reponseData = await response.json();
+
+    if(!response.ok) {
+      // error
+    }
+
+    const coaches = [];
+
+    for (const key in reponseData) {
+      const coach = {
+        id: key,
+        firstName: reponseData[key].firstName,
+        lastName: reponseData[key].lastName,
+        description: reponseData[key].description,
+        hourlyRate: reponseData[key].hourlyRate,
+        areas: reponseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches)
   }
 };
